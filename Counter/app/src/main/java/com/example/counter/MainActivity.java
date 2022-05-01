d4e568519779c9d428cca71f536f7b98f6aba68a
komalkr1 <komalriazkr1@gmail.com>
2022-05-01 18:16:12 +0500

    Complete Counter with increment Op

--- Counter/app/src/main/java/com/example/counter/MainActivity.java
+++ Counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.counter;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+    Button inc;
+    TextView counterVal;
+    Button reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button inc= findViewById(R.id.Increment_btn);
+        TextView counterVal= findViewById(R.id.textView);
+        Button reset=findViewById(R.id.reset_btn);
+
+        inc.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                counter ++;
+                counterVal.setText(Integer.toString(counter));
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                counter=0;
+                counterVal.setText(Integer.toString(counter));
+            }
+        });
     }
 }
