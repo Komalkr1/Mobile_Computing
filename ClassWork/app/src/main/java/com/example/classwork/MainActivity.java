f25f7300e5766c877d70bd6b15b06a906e341799
komalkr1 <komalriazkr1@gmail.com>
2022-06-09 22:51:42 +0500

    Dynamic Addition in ListView

--- ClassWork/app/src/main/java/com/example/classwork/MainActivity.java
+++ ClassWork/app/src/main/java/com/example/classwork/MainActivity.java
@@ -2,30 +2,54 @@ package com.example.classwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import android.widget.TextView;
 
 public class MainActivity<ArrayList> extends AppCompatActivity {
 
     ListView listView;
-    java.util.ArrayList<String> studentList=new  java.util.ArrayList<String>();
+    Button button2;
+    EditText textView;
+    java.util.ArrayList<String> studentList=new java.util.ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=findViewById(R.id.listView);
+        listView=findViewById(R.id.listView2);
+        button2=findViewById(R.id.button2);
+        textView=findViewById(R.id.textView2);
 
-        studentList.add("Saad");
-        studentList.add("Zahid");
-        studentList.add("Saeed");
-        studentList.add("Zainab");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, studentList);
+        textView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                textView.setText("");
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                studentList.add(textView.getText().toString());
+//                studentList.add("Saad");
+                arrayAdapter.notifyDataSetChanged();
+
+
+            }
+        });
+
+
 
         listView.setAdapter(arrayAdapter);
+
+
 
     }
 }
